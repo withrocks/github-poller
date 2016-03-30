89bd9c8969571625e6f0fb4afd94b94316f6a332
withrocks <withrocks@users.noreply.github.com>
2016-03-30 17:01:12 +0200

    Use 'If-None-Match' header to avoid limit

--- github_poller/cli.py
+++ github_poller/cli.py
@@ -9,18 +9,18 @@ from requests.auth import HTTPBasicAuth
 logger = logging.getLogger(__name__)
 STATE_FILE = ".github-poller"
 
-def save_state(sha):
+def save_state(etag, sha):
     with open(STATE_FILE, 'w+') as f:
-        f.write(sha + "\n")
+        f.write("{},{}\n".format(etag, sha))
 
 def read_state():
     if os.path.exists(STATE_FILE):
         with open(STATE_FILE, 'r') as f:
-            sha = f.read().rstrip()
-            logger.info("Read state from state file: {}".format(sha))
-            return sha
+            content = f.read().rstrip()
+            logger.info("Read state from state file: {}".format(content))
+            return content.split(",")
     else:
-        return None
+        return None, None
 
 @click.command()
 @click.argument('user')
@@ -41,10 +41,11 @@ def main(user, repo, branch, interval, command, oauth_user, oauth_token, level):
     The request limit is constrained for unauthorized tokens (currently 60 reqs/hour).
     Create an oauth token and use with client_id/client_secret to increase it to 5.000 reqs/hour.
     """
-    # TODO: Use conditonal requests (https://developer.github.com/v3/)
     MIN_INTERVAL = 1
     RATE_LIMIT_UNAUTH = 60
     RATE_LIMIT_AUTH = 5000
+    HEADER_REMAINING = "X-RateLimit-Remaining"
+    HEADER_LIMIT = "X-RateLimit-Limit"
 
     logging.basicConfig(level=level)
 
@@ -57,25 +58,33 @@ def main(user, repo, branch, interval, command, oauth_user, oauth_token, level):
     # rate_limit = RATE_LIMIT_UNAUTH if not token else RATE_LIMIT_AUTH
     # logger.warn("Poll rate may exceed the default rate limit")
 
-    last_sha = read_state()
-
+    etag, last_sha = read_state()
     url = "https://api.github.com/repos/{}/{}/branches/{}" \
             .format(user, repo, branch)
 
     while True:
-        response = requests.get(url, auth=auth)
-        if response.status_code != 200:
-            logger.warn("Status code {} != 200. Ignoring"
+        headers = {"If-None-Match": etag} if etag else None
+        response = requests.get(url, auth=auth, headers=headers)
+        etag = response.headers["ETag"]
+
+        logger.debug("API Limit: {}/{}".format(
+            response.headers[HEADER_REMAINING],
+            response.headers[HEADER_LIMIT]))
+
+        if response.status_code == 304:
+            logger.debug("No change (304)")
+        elif response.status_code != 200:
+            logger.warn("Status code {} != 200. Ignoring."
                     .format(response.status_code))
         else:
             response_obj = response.json()
             sha = response_obj["commit"]["sha"]
             if sha == last_sha:
-                logger.debug("No changes")
+                logger.debug("SHA is unchanged")
             else:
                 logger.info("SHA has changed. Firing event.")
                 os.system(command)
                 last_sha = sha
-                save_state(last_sha)
+                save_state(etag, last_sha)
         time.sleep(interval)
 
